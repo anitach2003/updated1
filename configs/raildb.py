d56fe500525e9d411ec05613b0757fae91173298
anitach2003 <123134603+anitach2003@users.noreply.github.com>
2026-02-16 16:57:17 +0330

    Update raildb.py

--- configs/raildb.py
+++ configs/raildb.py
@@ -7,8 +7,8 @@ epoch = 100
 batch_size = 32
 optimizer = 'Adam'    #['SGD','Adam']
 # learning_rate = 0.1
-learning_rate = 4e-4
-weight_decay = 1e-4
+learning_rate = 1e-4
+weight_decay = 1e-5
 momentum = 0.9
 
 scheduler = 'cos'     #['multi', 'cos']
